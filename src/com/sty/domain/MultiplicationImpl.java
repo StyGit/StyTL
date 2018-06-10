07f07f7a2cb31422089bc513cda2acaa376b84d2
Teja <Teja@Reddy>
2018-06-10 18:49:14 +0530

    develop MultiplicationImpl class

---
+++ src/com/sty/domain/MultiplicationImpl.java
@@ -0,0 +1,35 @@
+package com.sty.domain;
+
+public class MultiplicationImpl implements Multiplication {
+
+	@Override
+	public int mul(int firstVal, int secondVal) {
+		int result = firstVal * secondVal;
+		return result;
+	}
+
+	@Override
+	public String mul(String firstVal, String secondVal) {
+		//no code for this method
+		return "string can perform only add operation remaining arithmetic operations are not performed(string doesn't allow operators) ";
+	}
+
+	@Override
+	public float mul(float firstVal, float secondVal) {
+		float result = firstVal * secondVal;
+		return result;
+	}
+
+	@Override
+	public short mul(short firstVal, short secondVal) {
+		short result = (short) (firstVal * secondVal);
+		return result;
+	}
+
+	@Override
+	public byte mul(byte firstVal, byte secondVal) {
+		byte result = (byte) (firstVal * secondVal);
+		return result;
+	}
+
+}
