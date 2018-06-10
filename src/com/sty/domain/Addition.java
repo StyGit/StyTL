aa1b4104737002a74f2d3149d84e2cd8f4e0d377
SATYA <SATYA@SATYA-PC>
2018-06-10 16:48:40 +0530

    TL->Project Initiation

---
+++ src/com/sty/domain/Addition.java
@@ -0,0 +1,10 @@
+package com.sty.domain;
+
+public interface Addition {
+
+	public int add(int firstVal,int secondVal);
+	public String add(String firstVal,String secondVal);
+	public float add(float firstVal,float secondVal);
+	public short add(short firstVal,short secondVal);
+	public byte add(byte firstVal,byte secondVal);
+}
---
+++ src/com/sty/domain/Multiplication.java
@@ -0,0 +1,9 @@
+package com.sty.domain;
+
+public interface Multiplication {
+	public int mul(int firstVal,int secondVal);
+	public String mul(String firstVal,String secondVal);
+	public float mul(float firstVal,float secondVal);
+	public short mul(short firstVal,short secondVal);
+	public byte mul(byte firstVal,byte secondVal);
+}
